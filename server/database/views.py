343dcf4be87b89ac1238975ca1b543a668b8c1be
배성준 <52833902+vghjm@users.noreply.github.com>
2021-06-25 18:46:46 +0900

    카테고리 데이터 생성 및 테스트 완료

--- server/database/views.py
+++ server/database/views.py
@@ -1,3 +1,11 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from database.models import Category
 from django.shortcuts import render
 
 # Create your views here.
+def start(request):
+    Category(category_name="맥주").save()
+    Category(category_name="와인").save()
+    Category(category_name="캌테일").save()
+    return HttpResponse('success ')
