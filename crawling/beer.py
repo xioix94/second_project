73fbf0123b222fa639c743549a3a9b1cee82e4a1
xioix94 <53420414+xioix94@users.noreply.github.com>
2021-06-26 15:14:16 +0900

    맥주 크롤링 작업 중
    제곧내

--- crawling/beer.py
+++ crawling/beer.py
@@ -39,32 +39,49 @@ for item in file_list:
         content = f.readlines()
         # 각 맛 구분 찾아내기
         # 탄산감(sparkling) sp
-        for i in content[53]:
-            start = 0
-            end = 0
-            for i in range(len(content[53])):
-                if content[53][i] == '!':
-                    start = i + 4
-            for i in range(start, len(content[53])):
-                if content[53][i] == '번':
-                    end = i
-            sp.append((content[53][start:end]))
+        start = 0
+        end = 0
+        for j in range(len(content[53])):
+            if content[53][j] == '!':
+                start = j + 4
+                break
+        for j in range(start, len(content[53])):
+            if content[53][j] == '번':
+                end = j
+                break
+        sp.append((content[53][start:end]))
 
     
-        for i in content[54]:
-            start = 0
-            end = 0
-            for i in range(len(content[53])):
-                if content[54][i] == '!':
-                    start = i + 4
-            for i in range(start, len(content[53])):
-                if content[54][i] == '번':
-                    end = i
-            print(content[54][start:end])
-            # titles.append(content[53][start:end])
-            break
-        
+        start = 0
+        end = 0
+        for j in range(len(content[54])):
+            if content[54][j] == '!':
+                start = j + 4
+                break
+        for j in range(start, len(content[54])):
+            if content[54][j] == '번':
+                end = j
+                break
+        sp.append((content[54][start:end]))
+
+        sp = list(map(float, sp))
+        if sp[0] == sp[1] == 0:
+            sp = 0
+        else:
+            sp = round(sp[0] / (sp[0] + sp[1]), 2)
+
         # 달콤함(dry sweet) sw
+        start = 0
+        end = 0
+        for j in range(len(content[20])):
+            if content[20][j] == ':':
+                start = j+2
+                break
+        for j in range(start, len(content[20])):
+            if content[20][j] == '%':
+                end = j
+                break
+        round(content[20][start:end])
 
         # 씁쓸함(smooth tannic) bit
 
