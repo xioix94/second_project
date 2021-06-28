3dcecd0b441928dd9d7ef19460be8551701eab2c
배성준 <52833902+vghjm@users.noreply.github.com>
2021-06-28 17:34:06 +0900

    static 연결 추가
    logo 연결
    깨진 css 수정

--- server/app/urls.py
+++ server/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('404/', views.page_404),
     path('', views.index),
 ]
--- server/app/views.py
+++ server/app/views.py
@@ -2,5 +2,9 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def page_404(request):
+    return render(request, 'app/404.html')
+
+
 def index(request):
     return render(request, 'app/index.html')
