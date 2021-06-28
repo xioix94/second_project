d63221f89deb2b27b5e2478a126aee18ec575b31
배성준 <52833902+vghjm@users.noreply.github.com>
2021-06-28 17:39:58 +0900

    페이지 링크 연결

--- server/app/urls.py
+++ server/app/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('404/', views.page_404),
+    path('blog_single/', views.blog_single),
+    path('blog/', views.blog),
+    path('contact/', views.contact),
+    path('icons/', views.icons),
     path('', views.index),
+    path('login_check/', views.login_check),
+    path('login_form/', views.login_form),
+    path('product_single/', views.product_single),
+    path('product/', views.product),
 ]
--- server/app/views.py
+++ server/app/views.py
@@ -5,6 +5,30 @@ from django.shortcuts import render
 def page_404(request):
     return render(request, 'app/404.html')
 
+def blog_single(request):
+    return render(request, 'app/blog_single.html')
+
+def blog(request):
+    return render(request, 'app/blog.html')
+
+def contact(request):
+    return render(request, 'app/contact.html')
+
+def icons(request):
+    return render(request, 'app/icons.html')
 
 def index(request):
     return render(request, 'app/index.html')
+
+def login_check(request):
+    return render(request, 'app/login_check.html')
+
+def login_form(request):
+    return render(request, 'app/login_form.html')
+
+def product_single(request):
+    return render(request, 'app/product_single.html')
+
+def product(request):
+    return render(request, 'app/product.html')
+
