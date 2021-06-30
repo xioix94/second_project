91e104905d2e26cdb66e1ee4daa9caa460eb3a9b
xioix94 <53420414+xioix94@users.noreply.github.com>
2021-06-30 20:58:41 +0900

    내가했다

--- server/api/views.py
+++ server/api/views.py
@@ -3,31 +3,33 @@ from django.http import JsonResponse
 import re
 from app.models import User
 
-# 이메일 검증
-def email_valid(request):
+def email_valid_duplicate(request):
     email = request.GET.get('email')
-
-    ok = 'ok'
+    ok1 = 'ok'
+    ok2 = 'ok'
+    final_ok = 'ok'
     a = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
     result = a.match(email)
     if not result:
-        ok = 'not ok'
+        ok1 = 'not ok'
 
-    return JsonResponse({
-        'result': ok
-    })
-
-def email_duplicate(request):
-    email = request.GET.get('email')
-    ok = 'ok'
-    
     #데이터 베이스 조회
     users = User.objects.all()
     for user in users:
         if user.email == email:
-            ok = 'not ok'
+            ok2 = 'not ok'
             break
+   
+    if ok1 == 'not ok' or ok2 == 'not ok':
+        final_ok = 'not ok'
+    
+    return JsonResponse({
+        'result': final_ok
+    })
 
+
+def alias_valid(request):
+    ok = 'ok'
     return JsonResponse({
         'result': ok
     })
