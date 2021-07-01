0811b89572541a7c4f69c2e3008a63c4587087e8
ada-mj <79268185+ada-mj@users.noreply.github.com>
2021-07-01 09:29:36 +0900

    login_check

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('email_valid/', views.email_valid),
     path('email_duplicate/', views.email_duplicate),
-]
+    path('login/', views.login),
+    ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import re
 from app.models import User
 
@@ -31,3 +31,19 @@ def email_duplicate(request):
     return JsonResponse({
         'result': ok
     })
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'app/login_check.html', {})
+    else:
+        user_id = request.POST['email']
+        user_pw = request.POST['password']
+        
+        try:
+            member = User.objects.get(email=user_id,password=user_pw)
+        except:
+            return HttpResponse('로그인 실패')
+        else:
+            # request.session['email'] = user_id
+            return HttpResponse('로그인 성공')
