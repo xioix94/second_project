2c3616125c3e0873a9519cb3815e4f08e23e7d8c
xioix94 <53420414+xioix94@users.noreply.github.com>
2021-07-02 14:38:16 +0900

    index data number diplay & footer fix
    sdf

--- server/app/views.py
+++ server/app/views.py
@@ -39,7 +39,23 @@ def icons(request):
 
 
 def index(request):
-    return render(request, 'app/index.html')
+    products = Product.objects.all() 
+    comments = Product_Comment.objects.all()
+    beer_num, wine_num, cock_num, review_num = 0, 0, 0, 0 
+    
+    for product in products:
+        if product.category_id == 1:
+            beer_num += 1
+        elif product.category_id == 2:
+            wine_num += 1
+        else:
+            cock_num += 1
+        
+    for _ in comments:
+        review_num += 1
+    context = {'beer_num':beer_num, 'wine_num':wine_num, 'cock_num':cock_num, 'review_num':review_num}
+
+    return render(request, 'app/index.html', context)
 
 
 # 추천 페이지에 맥주 데이터 가져오기 (16개)
@@ -176,10 +192,8 @@ def userpage(request):
         'user_comments': user_comments,
     })
 
-
 def register(request):
     return render(request, 'app/register.html')
-
 
 def login(request):
     if request.method == 'GET':
@@ -197,10 +211,6 @@ def login(request):
             request.session['email'] = email
             return render(request, 'app/index.html')
 
-
-
 def comment_modify(request):
     user = User.objects.get(email=email)
     user_comments = Product_Comment.objects.filter(user_id=user.id).select_related('product')
-
-    
