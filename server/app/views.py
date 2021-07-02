fbe8c7128be7b293297c3783227f46daa955ef64
ada-mj <79268185+ada-mj@users.noreply.github.com>
2021-07-02 17:34:51 +0900

    ㄱㄱ
    ㄱㄱㄱㄱ

--- server/app/views.py
+++ server/app/views.py
@@ -21,6 +21,7 @@ def blog(request):
     category = request.GET.get('category')
 
     p_comments = Product_Comment.objects.none()
+    
     if category:
         category_id = Category.objects.get(name=category)
         products = Product.objects.filter(category_id=category_id)
@@ -29,14 +30,32 @@ def blog(request):
     else:
         p_comments = Product_Comment.objects.select_related()
 
-    return render(request, 'app/blog.html', {
-        'p_comments': p_comments,
-        'category': category})
+    page = request.GET.get('page')
 
+    if not page:
+        page = '1'
+    
+    p = Paginator(p_comments, 10)
+    
+    pp_c = p.page(page)
+
+    start_page = (int(page) - 1) // 10 * 10 + 1
+    end_page = start_page + 9
+
+    if end_page > p.num_pages:
+        end_page = p.num_pages
+
+    context = {
+        'pp_c' : pp_c,
+        'pagination' : range(start_page, end_page + 1),
+        'p_comments': p_comments,
+        'category': category}
+    # return render(request, 'app/blog.html', { 'p_comments': p_comments, 'category': category })
+    return render(request, 'app/blog.html', context)
 
 def contact(request):
     return render(request, 'app/contact.html')
-
+    
 
 def icons(request):
     return render(request, 'app/icons.html')
@@ -49,17 +68,17 @@ def index(request):
 # 추천 페이지에 맥주 데이터 가져오기 (16개)
 def recommand(request):
     products = Product.objects.order_by('?')[:16]
-
+        
     return render(request, 'app/recommand.html', {
         'products': products
     })
 
 
-# 추천 페이지 결과를 이용 -> 머신러닝(클러스터링) -> 결과값과 동일한 군집의 제품 데이터 가져오기 (16개)
+# 추천 페이지 결과를 이용 -> 머신러닝(클러스터링) -> 결과값과 동일한 군집의 제품 데이터 가져오기 (16개) 
 def recommand_result(request):
     # 머신러닝 나온 군집 안의 제품으로 줘야 함                     (수정 필요)
     products = Product.objects.order_by('?')[:16]
-
+        
     return render(request, 'app/recommand_result.html', {
         'products': products
     })
@@ -72,7 +91,7 @@ def login_form(request):
 def product_single(request):
     try:
         product_id = request.GET.get('p_id')
-
+        
         product = Product.objects.get(id=product_id)
         product_comment_list = Product_Comment.objects.filter(product_id=product.id).select_related('user')
 
@@ -117,7 +136,6 @@ def product(request):
         category_id = Category.objects.get(name=category)
         products = Product.objects.filter(category_id=category_id)
     else:
-        category = "all"
         products = Product.objects.all()
 
     return render(request, 'app/product.html', {
@@ -145,7 +163,7 @@ def profile(request):
     else:
         nickname = request.POST['nickname']
         password = request.POST['password']
-
+        
         try:
             email = request.session.get('email')
             user = User.objects.get(email=email)
@@ -168,10 +186,10 @@ def to_members_form(request):
 
 def userpage(request):
     email = request.GET.get('email')
-
+    
     user = User.objects.get(email=email)
 
-
+    
     user_comments = Product_Comment.objects.filter(user_id=user.id).select_related('product')
 
 
@@ -192,7 +210,7 @@ def login(request):
     else:
         email = request.POST['email']
         password = request.POST['password']
-
+        
         try:
             member = User.objects.get(email=email,password=password)
         except:
@@ -204,13 +222,11 @@ def login(request):
             return render(request, 'app/index.html')
 
 
-
 def comment_modify(request):
-
 
     if request.method == "POST":
         form = userpage(request.POST, instance=user_comments)
-
+        
         if form.is_valid():
             user_comments = form.save(commit=False)
             user_comments.save()
@@ -222,15 +238,8 @@ def comment_modify(request):
         comment_id = request.GET.get('comment_id')
         user_comment = Product_Comment.objects.get(id=comment_id)
 
-        return render(request, 'app/freewrite.html')
+        return render(request, 'app/')
 
     else:
         pass
 
-
-
-
-
-def logout(request):
-    request.session.clear()
-    return redirect('/')
