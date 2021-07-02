00c14f8748649a8251828f6d1c056db68887cb97
배성준 <52833902+vghjm@users.noreply.github.com>
2021-07-02 12:04:21 +0900

    add consumers and update setting

--- server/config/asgi.py
+++ server/config/asgi.py
@@ -1,13 +1,15 @@
 import os
 
 import django
+from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
-  "http": AsgiHandler(),
-  # Just HTTP for now. (We can add other protocols later.)
+    "http": AsgiHandler(),
+    "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
 })
