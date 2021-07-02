1524f4a6c2c542c86b93350106067f5389144bea
배성준 <52833902+vghjm@users.noreply.github.com>
2021-07-02 10:52:19 +0900

    board single

--- server/board/views.py
+++ server/board/views.py
@@ -8,3 +8,13 @@ def board(request):
     return render(request, "app/board.html", {
         'boards': boards
     })
+
+def single(request):
+    board_id = request.GET.get('id')
+    board = Board.objects.select_related().get(id=board_id)
+    board_comments = Board_Comment.objects.filter(board_id=board_id).select_related('user')
+
+    return render(request, "app/board_single.html", {
+        'board': board,
+        'board_comments': board_comments,
+    })
