5b5022fe79a46ad7825b2ddc39f0f59590fd90a6
xioix94 <53420414+xioix94@users.noreply.github.com>
2021-07-05 03:38:35 +0900

    여러가지
    메인 이미지, 회원가입 암호화
    로그인 복호화 코드 준비

--- server/api/urls.py
+++ server/api/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register),
-    path('login/', views.login),
     path('email_check/', views.email_check),
     path('password_valid/', views.password_valid),
     path('alias_valid/', views.alias_valid),
